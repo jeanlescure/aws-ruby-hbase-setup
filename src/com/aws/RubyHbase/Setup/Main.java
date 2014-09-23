60fb43789af45f59179ca7e306cc2d9b8fde2c7a
Jean Lescure <jean@agilityfeat.com>
2014-09-23 13:13:17 -0600

    Running specific cp for bin.

--- src/com/aws/RubyHbase/Setup/Main.java
+++ src/com/aws/RubyHbase/Setup/Main.java
@@ -28,7 +28,9 @@ public class Main {
         // Get and setup Ruby HBase daemon from GitHub
         pList.add(  new ProcessBuilder("git", "clone", "https://github.com/jeanlescure/aws-ruby-hbase-daemon.git"),
                     defaultFile   );
-        pList.add(  new ProcessBuilder("bash", "-c", "\"cp -f aws-ruby-hbase-daemon/bin/* hbase/bin/\""),
+        pList.add(  new ProcessBuilder("cp", "-f", "aws-ruby-hbase-daemon/bin/hbase", "hbase/bin/hbase"),
+                    defaultFile   );
+        pList.add(  new ProcessBuilder("cp", "-f", "aws-ruby-hbase-daemon/bin/rb-daemon.rb", "hbase/bin/rb-daemon.rb"),
                     defaultFile   );
         pList.add(  new ProcessBuilder("cp", "-r", "aws-ruby-hbase-daemon/ruby/daemon", "hbase/lib/ruby/"),
                     defaultFile   );
